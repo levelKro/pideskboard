c6f821704891b51abf1db431db3880c56083d802
levelKro <levelkro@yahoo.ca>
2021-05-04 20:53:44 -0400

    Update dm_start.sh

--- sh/dm_start.sh
+++ sh/dm_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /home/pi/pideskboard/sh/cli_splash.sh
 #/home/pi/pideskboard/sh/cli_icon.sh 60000 hourglass 0
-nohup /home/pi/pideskboard/sh/cli_bt.sh >/dev/null 2>&1 &
+#nohup /home/pi/pideskboard/sh/cli_bt.sh >/dev/null 2>&1 &
 /home/pi/pideskboard/sh/dm_monitor.sh &
 #nohup php /home/pi/pideskboard/php/espeak.php >/dev/null 2>&1 &
 #nohup php /home/pi/pideskboard/php/cron.php >/dev/null 2>&1 &
