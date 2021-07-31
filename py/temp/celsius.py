0b925aed29606d73950cf175cd4068df2973c3af
levelKro <levelkro@yahoo.ca>
2021-07-31 03:49:16 -0400

    Add temperature sensor support
    Based on DS18B20 sensor code. Compatible with the Home made or with "keyes" module.

---
+++ py/temp/celsius.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import time
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        degree_sign = u"\N{DEGREE SIGN}"
+        output = str(round(temp_c,0)).replace(".0","") + degree_sign + "C"
+        return output.encode("utf-8")
+	
+print(read_temp())	
