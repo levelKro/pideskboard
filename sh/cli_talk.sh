4daf0d8d59ec0d7be41e46422fc102bb68a72236
levelKro <levelkro@yahoo.ca>
2021-12-12 01:36:04 -0500

    Update cli_talk.sh

--- sh/cli_talk.sh
+++ sh/cli_talk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Other parameter : -p 30
 #/home/pi/pideskboard/cli_icon.sh 5000 sound
-nohup /usr/bin/espeak -s 100 -l 1 -a 125 -b 1 -v $1 "$2" >/dev/null 2>&1 &
+nohup /usr/bin/espeak-ng -s 100 -l 1 -a 95 -b 1 -v $1 "$2" >/dev/null 2>&1 &
